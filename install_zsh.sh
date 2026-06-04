2cdd817ca2a137ddfda8b726205f31caee9b8d22
Jean-Paul Miéville <jpmieville@gmail.com>
2026-06-04 11:51:10 +0200

    Add Powerlevel10k theme and zsh plugins

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,6 +4,7 @@ sudo apt-get install zsh
 sudo apt-get install fonts-powerline
 chsh -s /bin/zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
